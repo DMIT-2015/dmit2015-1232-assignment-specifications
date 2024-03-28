6b5803fb05e7f8cc4ca01bd791dc92b12f72266e
Sam Wu <swu@nait.ca>
2024-03-28 17:40:38 -0600

    Removed add-null-date-lookup-values code from fill-dimdates

--- assignment08files/northwind-etl-drop-foreign-keys.sql
+++ assignment08files/northwind-etl-drop-foreign-keys.sql
@@ -2,6 +2,6 @@ Alter Table [dbo].DimEmployees Drop Constraint FK_DimEmployees_DimEmployees
 Alter Table [dbo].FactOrders Drop Constraint FK_FactOrders_DimCustomers
 Alter Table [dbo].FactOrders Drop Constraint FK_FactOrders_DimEmployees
 Alter Table [dbo].FactOrders Drop Constraint FK_FactOrders_DimProducts
-Alter Table [dbo]. FactOrders Drop Constraint FK_FactOrders_DimDates_OrderDate
+Alter Table [dbo].FactOrders Drop Constraint FK_FactOrders_DimDates_OrderDate
 Alter Table [dbo].FactOrders Drop Constraint FK_FactOrders_DimDates_RequiredDate
 Alter Table [dbo].FactOrders Drop Constraint FK_FactOrders_DimDates_ShippedDate
